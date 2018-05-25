c055f077a63c61047d457d123da3d19ec5d8208c
marcosvporto <39508000+marcosvporto@users.noreply.github.com>
2018-05-24 22:05:50 -0300

    Add files via upload

--- 1413428-1413383/src/controller/Partida.java
+++ 1413428-1413383/src/controller/Partida.java
@@ -12,27 +12,27 @@ public class Partida {
 		c = t.getMatrizCasas();
 		for(int i = 0;i<8;i++) {
 			switch(i) {
-			case 0: c[i][0].setPeca(new Torre( 'p', 0, i));break;
-			case 1: c[i][0].setPeca(new Bispo( 'p', 0, i));break;
-			case 2: c[i][0].setPeca(new Cavalo('p', 0, i));break;
-			case 3: c[i][0].setPeca(new Rei(   'p', 0, i));break;
-			case 4: c[i][0].setPeca(new Dama(  'p', 0, i));break;
-			case 5: c[i][0].setPeca(new Cavalo('p', 0, i));break;
-			case 6: c[i][0].setPeca(new Bispo( 'p', 0, i));break;
-			case 7: c[i][0].setPeca(new Torre( 'p', 0, i));break;
+			case 0: c[i][0].setPeca(new Torre(  'p', 0, i));break;
+			case 1: c[i][0].setPeca(new Cavalo( 'p', 0, i));break;
+			case 2: c[i][0].setPeca(new Bispo(  'p', 0, i));break;
+			case 3: c[i][0].setPeca(new Dama(   'p', 0, i));break;
+			case 4: c[i][0].setPeca(new Rei(    'p', 0, i));break;
+			case 5: c[i][0].setPeca(new Bispo(  'p', 0, i));break;
+			case 6: c[i][0].setPeca(new Cavalo( 'p', 0, i));break;
+			case 7: c[i][0].setPeca(new Torre(  'p', 0, i));break;
 			
 			}
 		}
 		for(int i = 0;i<8;i++) {
 			switch(i) {
-			case 0: c[i][7].setPeca(new Torre( 'b', 7 , i));break;
-			case 1: c[i][7].setPeca(new Bispo( 'b', 7 , i));break;
-			case 2: c[i][7].setPeca(new Cavalo('b', 7 , i));break;
-			case 3: c[i][7].setPeca(new Dama(  'b', 7 , i));break;
-			case 4: c[i][7].setPeca(new Rei(   'b', 7 , i));break;
-			case 5: c[i][7].setPeca(new Cavalo('b', 7 , i));break;
-			case 6: c[i][7].setPeca(new Bispo( 'b', 7 , i));break;
-			case 7: c[i][7].setPeca(new Torre( 'b', 7 , i));break;
+			case 0: c[i][7].setPeca(new Torre(  'b', 7 , i));break;
+			case 1: c[i][7].setPeca(new Cavalo( 'b', 7 , i));break;
+			case 2: c[i][7].setPeca(new Bispo(  'b', 7 , i));break;
+			case 3: c[i][7].setPeca(new Rei(    'b', 7 , i));break;
+			case 4: c[i][7].setPeca(new Dama(   'b', 7 , i));break;
+			case 5: c[i][7].setPeca(new Bispo(  'b', 7 , i));break;
+			case 6: c[i][7].setPeca(new Cavalo( 'b', 7 , i));break;
+			case 7: c[i][7].setPeca(new Torre(  'b', 7 , i));break;
 			}
 		}
 		for(int i=0;i<8;i++) {
